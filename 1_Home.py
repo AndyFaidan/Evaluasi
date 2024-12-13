cbff77d53fecbc9047b83958bc38f8b2e0cc1603
andy sofyan guspriyanto <129332680+AndyFaidan@users.noreply.github.com>
2024-12-13 20:41:10 +0700

    Update 1_Home.py

--- 1_Home.py
+++ 1_Home.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 from streamlit_apexjs import st_apexcharts
-import matplotlib.pyplot as plt
 import numpy as np
-import plotly.figure_factory as ff
 import altair as alt
 
 # Set page configuration
