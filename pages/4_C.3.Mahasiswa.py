4cebc4578c8cf07674657ed872cb1b7d7d628a72
andy sofyan guspriyanto <129332680+AndyFaidan@users.noreply.github.com>
2024-12-16 18:11:17 +0700

    Update 4_C.3.Mahasiswa.py

--- pages/4_C.3.Mahasiswa.py
+++ pages/4_C.3.Mahasiswa.py
@@ -179,13 +179,13 @@ with col1:
     )
     # Update layout to center the title and position the legend at the bottom
     fig_donut.update_layout(
-        title_x=0.1,  # Centers the title
+        title_x=0.2,  # Centers the title
         legend_title="Indikator",  # Title for the legend
         legend_orientation="h",  # Horizontal legend
-        legend_yanchor="bottom",  # Aligns legend at the bottom
+        legend_yanchor="bottom","center",  # Aligns legend at the bottom
         legend_y=-0.4,  # Moves the legend below the chart
         legend_x=0.4,  # Centers the legend horizontally
-        legend_xanchor="right"  # Ensures that the legend is anchored in the center
+        legend_xanchor="center"  # Ensures that the legend is anchored in the center
     )
     # Display the donut chart
     st.plotly_chart(fig_donut)
