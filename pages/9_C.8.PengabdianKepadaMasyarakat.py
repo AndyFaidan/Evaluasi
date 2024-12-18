86d4a8fd17a22f4bc738939ff58586d9f61e4c0f
andy sofyan guspriyanto <129332680+AndyFaidan@users.noreply.github.com>
2024-12-19 01:42:18 +0700

    Update 9_C.8.PengabdianKepadaMasyarakat.py

--- pages/9_C.8.PengabdianKepadaMasyarakat.py
+++ pages/9_C.8.PengabdianKepadaMasyarakat.py
@@ -149,11 +149,15 @@ with col1:
             title=f"Persentase Terpenuhi dan Tidak Terpenuhi untuk Pertanyaan",
             color_discrete_sequence=px.colors.sequential.Sunset
         )
-        # Update layout to center the title and position the legend at the bottom
+     # Update layout to center the title and position the legend at the bottom
         fig_donut.update_layout(
+            title_x=0.2,  # Centers the title
             legend_title="Indikator",  # Title for the legend
             legend_orientation="h",  # Horizontal legend
-            legend_yanchor="middle",  # Aligns legend at the bottom
+            legend_yanchor="bottom",  # Aligns legend at the bottom
+            legend_y=-0.5,  # Moves the legend below the chart
+               legend_x=0.5,  # Centers the legend horizontally
+             legend_xanchor="center"  # Ensures that the legend is anchored in the center
         )
         # Display the donut chart
         st.plotly_chart(fig_donut, use_container_width=True)
