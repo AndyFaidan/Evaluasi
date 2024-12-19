42c15dcbd3184fa312836152bf932ac64d6b5441
andy sofyan guspriyanto <129332680+AndyFaidan@users.noreply.github.com>
2024-12-19 22:08:56 +0700

    Update 2_C.1.VisidanMisi.py

--- pages/2_C.1.VisidanMisi.py
+++ pages/2_C.1.VisidanMisi.py
@@ -9,8 +9,11 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
-# Judul aplikasi
-st.title("📊 Survey Pemahaman Dosen, Tendik Dan Mahasiswa Terhadap VMTS UPPS Dan PS")
+# Menampilkan judul aplikasi di tengah
+st.markdown("""
+    <h2 style="text-align: center;">📊 Survey Pemahaman Dosen, Tendik Dan Mahasiswa Terhadap VMTS UPPS Dan PS</h2>
+""", unsafe_allow_html=True)
+
 
 # Fungsi untuk memuat data dengan caching
 @st.cache_data
