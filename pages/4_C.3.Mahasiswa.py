f4b29e7311e7f3c39f41471147f793bd91902316
andy sofyan guspriyanto <129332680+AndyFaidan@users.noreply.github.com>
2025-01-03 20:32:10 +0700

    Update 4_C.3.Mahasiswa.py

--- pages/4_C.3.Mahasiswa.py
+++ pages/4_C.3.Mahasiswa.py
@@ -4,14 +4,14 @@ import plotly.express as px
 
 # Set page configuration
 st.set_page_config(
-    page_title="Survey Kepuasan Layanan Mahasiswa (T-ERRA)",
+    page_title="Survey Kepuasan Layanan Mahasiswa",
     layout="wide",
     initial_sidebar_state="collapsed",
 )
 
 # Menampilkan judul aplikasi di tengah
 st.markdown("""
-    <h2 style="text-align: center;">Survey Kepuasan Layanan Mahasiswa (T-ERRA)</h2>
+    <h2 style="text-align: center;">Survey Kepuasan Layanan Mahasiswa</h2>
 """, unsafe_allow_html=True)
 
 st.divider()
